a1af423a765a1b7eec4074cb350b8ec1718668aa
decagraff <Decagraff@gmail.com>
2020-06-12 19:10:25 -0500

    Detalle_Michi

--- app/src/main/java/com/michifinder/Detalle_Michi.kt
+++ app/src/main/java/com/michifinder/Detalle_Michi.kt
@@ -3,9 +3,9 @@ package com.michifinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Detalle_Michi : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detalle__michi)
     }
 }
